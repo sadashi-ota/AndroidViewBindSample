c41e60044a74cd0671a0e78e88ee997f59b39639
Sadashi Ota <sadashi.ota.dev@gmail.com>
2019-12-05 01:53:49 +0900

    How to use Butter Knife.

--- app/src/main/java/jp/sadashi/sample/viewbind/MainActivity.kt
+++ app/src/main/java/jp/sadashi/sample/viewbind/MainActivity.kt
@@ -2,11 +2,26 @@ package jp.sadashi.sample.viewbind
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import butterknife.BindView
+import butterknife.ButterKnife
+import butterknife.OnClick
 
 class MainActivity : AppCompatActivity() {
+    @BindView(R.id.submit_button)
+    lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ButterKnife.bind(this)
+        println("Button is $button")
+    }
+
+    @OnClick(R.id.submit_button)
+    fun submit(view: View) {
+        // do something
+        println("Click $view")
     }
 }
