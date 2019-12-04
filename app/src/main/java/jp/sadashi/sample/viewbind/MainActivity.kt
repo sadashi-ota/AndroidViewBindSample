4f59d991bdd4a94a9af03e59e85e03164f1335de
Sadashi Ota <sadashi.ota.dev@gmail.com>
2019-12-05 01:09:19 +0900

    How to use findViewById.

--- app/src/main/java/jp/sadashi/sample/viewbind/MainActivity.kt
+++ app/src/main/java/jp/sadashi/sample/viewbind/MainActivity.kt
@@ -2,11 +2,22 @@ package jp.sadashi.sample.viewbind
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button = findViewById(R.id.submit_button)
+        button.setOnClickListener(::submit)
+    }
+
+    private fun submit(view: View) {
+        // do something
+        println("Click $view")
     }
 }
