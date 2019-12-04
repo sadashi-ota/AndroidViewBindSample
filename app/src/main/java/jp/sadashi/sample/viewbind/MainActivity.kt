fec66303d86d8d11d293e446f1a9b4f27026b2bc
Sadashi Ota <sadashi.ota.dev@gmail.com>
2019-12-05 02:08:12 +0900

    How to use kotlin android extensions.

--- app/src/main/java/jp/sadashi/sample/viewbind/MainActivity.kt
+++ app/src/main/java/jp/sadashi/sample/viewbind/MainActivity.kt
@@ -2,11 +2,20 @@ package jp.sadashi.sample.viewbind
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        submit_button.setOnClickListener(::submit)
+        println("Button is $submit_button")
+    }
+
+    private fun submit(view: View) {
+        // do something
+        println("Click $view")
     }
 }
