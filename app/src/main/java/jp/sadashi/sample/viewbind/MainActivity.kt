d59068d20345c190f7edaeefdde797ef3c2b7fd1
Sadashi Ota <sadashi.ota.dev@gmail.com>
2019-12-17 03:05:09 +0900

    How to use view binding.

--- app/src/main/java/jp/sadashi/sample/viewbind/MainActivity.kt
+++ app/src/main/java/jp/sadashi/sample/viewbind/MainActivity.kt
@@ -2,11 +2,20 @@ package jp.sadashi.sample.viewbind
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import jp.sadashi.sample.viewbind.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.submitButton.setOnClickListener(::submit)
+    }
+
+    private fun submit(view: View) {
+        // do something
+        println("Click $view")
     }
 }
